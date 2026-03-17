93844b3e6633a8b717c82332db913aa8a1ba79ad
Yuseokshin <a01058031047@gmail.com>
2026-03-18 00:00:07 +0900

    Refactor multiple main functions in P97_1.cpp

--- Week3/P97_1.cpp
+++ Week3/P97_1.cpp
@@ -1,3 +1,4 @@
+//(1)
 #include <iostream>
 int main()
 {
@@ -9,4 +10,61 @@ int main()
         }
         std::cout << "\n";
     }
+}
+
+//(2)
+#include <iostream>
+
+int main()
+{
+    for(int i = 1; i <=5; ++i)
+    {
+
+        for(int j = 5; j >= i; --j)
+        {
+            std::cout << j;
+        }
+        std::cout << "\n";
+    }   
+}
+
+//(3)
+#include <iostream>
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+    {
+        // std::cout << i << "\n";
+        for(int j = 4; j >= i; j--)
+        {
+            std::cout << " ";
+        }
+        for (int l = 0; l <= i -1; l++)
+        {
+            std::cout << i - l;
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
+
+//(4)
+#include <iostream>
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+    {
+        for (int k = 4; k >= i; k--)
+        {
+            std::cout << " ";
+        }
+        for (int s=0; s<i; s++)
+        {
+            std::cout << i - s;
+        }
+        for (int j=2; j<=i; j++)
+            std::cout <<j;
+        std::cout << "\n";
+    }
+    return 0;
 }
